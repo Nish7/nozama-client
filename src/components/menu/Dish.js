469328984e344a4fd2d440cc2786b157732d4ab2
Bhavya Muni <bhavyamuni@outlook.com>
2023-04-12 12:43:27 -0400

    dollars

--- src/components/menu/Dish.js
+++ src/components/menu/Dish.js
@@ -1,100 +1,104 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 import {
-	addToTray,
-	addQuantity,
-	removeQuantity,
-	removeFromTray,
-} from '../../actions/tray';
+  addToTray,
+  addQuantity,
+  removeQuantity,
+  removeFromTray,
+} from "../../actions/tray";
 
 function Dish({
-	dish: { veg, dishName: name, _id: id, price, jain, vegan, featured },
-	addToTray,
-	addQuantity,
-	removeQuantity,
-	removeFromTray,
-	tray: { items },
+  dish: { veg, dishName: name, _id: id, price, jain, vegan, featured },
+  addToTray,
+  addQuantity,
+  removeQuantity,
+  removeFromTray,
+  tray: { items },
 }) {
-	const dishOptions = `${jain ? '(Jain' : ''} ${jain && vegan ? ',' : ''} ${
-		vegan ? 'Vegan' : ''
-	} ${jain || vegan ? 'Available)' : ''}`;
+  const dishOptions = `${jain ? "(Jain" : ""} ${jain && vegan ? "," : ""} ${
+    vegan ? "Vegan" : ""
+  } ${jain || vegan ? "Available)" : ""}`;
 
-	let trayButtons = '';
+  let trayButtons = "";
 
-	const foundItem = items.find((item) => item.id === id);
+  const foundItem = items.find((item) => item.id === id);
 
-	if (foundItem) {
-		trayButtons = (
-			<div className='quantity-buttons'>
-				<button
-					className='btn-remove'
-					onClick={(e) =>
-						foundItem.quantity !== 1 ? removeQuantity(id) : removeFromTray(id)
-					}>
-					-
-				</button>
-				<button className='btn-quantity'>{foundItem.quantity}</button>
+  if (foundItem) {
+    trayButtons = (
+      <div className="quantity-buttons">
+        <button
+          className="btn-remove"
+          onClick={(e) =>
+            foundItem.quantity !== 1 ? removeQuantity(id) : removeFromTray(id)
+          }
+        >
+          -
+        </button>
+        <button className="btn-quantity">{foundItem.quantity}</button>
 
-				<button className='btn-add' onClick={(e) => addQuantity(id)}>
-					+
-				</button>
-			</div>
-		);
-	} else {
-		trayButtons = (
-			<div className='food-cart'>
-				<button
-					onClick={(e) => addToTray(id)}
-					className='btn btn-outline-dark btn-cart'>
-					+
-				</button>
-			</div>
-		);
-	}
+        <button className="btn-add" onClick={(e) => addQuantity(id)}>
+          +
+        </button>
+      </div>
+    );
+  } else {
+    trayButtons = (
+      <div className="food-cart">
+        <button
+          onClick={(e) => addToTray(id)}
+          className="btn btn-outline-dark btn-cart"
+        >
+          +
+        </button>
+      </div>
+    );
+  }
 
-	return (
-		<div>
-			<div className='food'>
-				<div className='food-info'>
-					<h4>
-						<span>
-							<i
-								style={{
-									color: veg ? 'green' : 'red',
-									fontSize: '12px',
-								}}
-								className='fas fa-circle'></i>
-						</span>
-						{name}
-						{featured && (
-							<span>
-								<i
-									style={{
-										fontSize: '20px',
-										color: '#FFCC32',
-										marginLeft: '5px',
-									}}
-									className='fas fa-star'></i>
-							</span>
-						)}
-					</h4>
-					<small>{dishOptions}</small>
-					<p>₹{price}</p>
-				</div>
+  return (
+    <div>
+      <div className="food">
+        <div className="food-info">
+          <h4>
+            <span>
+              <i
+                style={{
+                  color: veg ? "green" : "red",
+                  fontSize: "12px",
+                }}
+                className="fas fa-circle"
+              ></i>
+            </span>
+            {name}
+            {featured && (
+              <span>
+                <i
+                  style={{
+                    fontSize: "20px",
+                    color: "#FFCC32",
+                    marginLeft: "5px",
+                  }}
+                  className="fas fa-star"
+                ></i>
+              </span>
+            )}
+          </h4>
+          <small>{dishOptions}</small>
+          <p>${price}</p>
+        </div>
 
-				{trayButtons}
-			</div>
-		</div>
-	);
+        {trayButtons}
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => ({
-	tray: state.tray,
+  tray: state.tray,
 });
 
 export default connect(mapStateToProps, {
-	addToTray,
-	addQuantity,
-	removeQuantity,
-	removeFromTray,
+  addToTray,
+  addQuantity,
+  removeQuantity,
+  removeFromTray,
 })(Dish);
