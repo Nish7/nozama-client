b7dbaf8c80e2a616a5fa15c5b12b288155234ff5
Bhavya Muni <bhavya.muni@ryerson.ca>
2023-04-12 18:16:50 -0400

    Bhavya edits COMPLETED 😎 (#1)
    * removed store info
    
    * dollars
    
    * edit dishes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,57 +1,60 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withRouter, Link } from 'react-router-dom';
-import { getRestaurant } from '../../actions/restaurant';
-import { logout } from '../../actions/auth';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter, Link } from "react-router-dom";
+import { getRestaurant } from "../../actions/restaurant";
+import { logout } from "../../actions/auth";
 
 const Navbar = ({
-	auth: { isAuthenticated, loading },
-	tableNum,
-	logout,
-	restaurant,
-	getRestaurant,
-	location,
-	history,
+  auth: { isAuthenticated, loading },
+  tableNum,
+  logout,
+  restaurant,
+  getRestaurant,
+  location,
+  history,
 }) => {
-	useEffect(() => {
-		if (!restaurant) return getRestaurant();
-	}, [restaurant, getRestaurant]);
+  useEffect(() => {
+    if (!restaurant) return getRestaurant();
+  }, [restaurant, getRestaurant]);
 
-	const currPath = location.pathname.split('/')[1];
-	const currAuthPath = location.pathname.split('/')[2];
+  const currPath = location.pathname.split("/")[1];
+  const currAuthPath = location.pathname.split("/")[2];
 
-	const guestLinks = (
-		<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-			<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-				<li className='nav-item'>
-					<Link
-						className={`nav-link ${currPath === 'menu' && 'active'}`}
-						aria-current='page'
-						to='/catalog'>
-						Catalog
-					</Link>
-				</li>
-				<li className='nav-item'>
-					<Link
-						className={`nav-link ${currPath === 'tray' && 'active'}`}
-						aria-current='page'
-						to='/cart'>
-						Cart
-					</Link>
-				</li>
-			</ul>
-			<ul className='nav navbar-nav navbar-right'>
-				<li className='nav-item'>
-					<Link
-						className={`nav-link ${currPath === 'admin' && 'active'}`}
-						to='/admin'
-						tabIndex='-1'
-						aria-disabled='true'>
-						Admin
-					</Link>
-				</li>
-				{/* <li className='nav-item'>
+  const guestLinks = (
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${currPath === "menu" && "active"}`}
+            aria-current="page"
+            to="/catalog"
+          >
+            Catalog
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${currPath === "tray" && "active"}`}
+            aria-current="page"
+            to="/cart"
+          >
+            Cart
+          </Link>
+        </li>
+      </ul>
+      <ul className="nav navbar-nav navbar-right">
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${currPath === "admin" && "active"}`}
+            to="/admin"
+            tabIndex="-1"
+            aria-disabled="true"
+          >
+            Admin
+          </Link>
+        </li>
+        {/* <li className='nav-item'>
 					<Link
 						className={`nav-link ${!currPath && 'active'}`}
 						to='/'
@@ -60,107 +63,114 @@ const Navbar = ({
 						Table {tableNum ? `Number : ${tableNum}` : ''}
 					</Link>
 				</li> */}
-			</ul>
-		</div>
-	);
+      </ul>
+    </div>
+  );
 
-	const authLinks = (
-		<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-			<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-				<li className='nav-item'>
-					<Link
-						className={`nav-link ${currAuthPath === 'orders' && 'active'}`}
-						aria-current='page'
-						to='/admin/orders'>
-						Orders
-					</Link>
-				</li>
-				<li className='nav-item'>
-					<Link
-						className={`nav-link ${currAuthPath === 'dishes' && 'active'}`}
-						aria-current='page'
-						to='/admin/products'>
-						Products
-					</Link>
-				</li>
-				<li className='nav-item'>
+  const authLinks = (
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${currAuthPath === "orders" && "active"}`}
+            aria-current="page"
+            to="/admin/orders"
+          >
+            Orders
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${currAuthPath === "dishes" && "active"}`}
+            aria-current="page"
+            to="/admin/products"
+          >
+            Products
+          </Link>
+        </li>
+        {/* <li className='nav-item'>
 					<Link
 						className={`nav-link ${currAuthPath === 'restaurant' && 'active'}`}
 						aria-current='page'
 						to='/admin/store'>
 						Store Info
 					</Link>
-				</li>
-				<li className='nav-item'>
-					<Link
-						className={`nav-link ${currAuthPath === 'categories' && 'active'}`}
-						aria-current='page'
-						to='/admin/categories'>
-						Categories
-					</Link>
-				</li>
-			</ul>
-			<ul className='nav navbar-nav navbar-right'>
-				<li className='nav-item '>
-					<Link
-						className={`nav-link ${currAuthPath === 'account' && 'active'}`}
-						aria-current='page'
-						to='/admin/account'>
-						Account <i className='fas fa-user'></i>
-					</Link>
-				</li>
-				<li className='nav-item'>
-					<a
-						onClick={(e) => {
-							logout();
-							history.push('/admin');
-						}}
-						className='nav-link'
-						aria-current='page'
-						href='#!'>
-						Logout <i className='fas fa-sign-out-alt'></i>
-					</a>
-				</li>
-			</ul>
-		</div>
-	);
+				</li> */}
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${currAuthPath === "categories" && "active"}`}
+            aria-current="page"
+            to="/admin/categories"
+          >
+            Categories
+          </Link>
+        </li>
+      </ul>
+      <ul className="nav navbar-nav navbar-right">
+        <li className="nav-item ">
+          <Link
+            className={`nav-link ${currAuthPath === "account" && "active"}`}
+            aria-current="page"
+            to="/admin/account"
+          >
+            Account <i className="fas fa-user"></i>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <a
+            onClick={(e) => {
+              logout();
+              history.push("/admin");
+            }}
+            className="nav-link"
+            aria-current="page"
+            href="#!"
+          >
+            Logout <i className="fas fa-sign-out-alt"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 
-	return (
-		<nav className='navbar navbar-expand-lg navbar-light bg-light'>
-			<div className='container-fluid'>
-				<Link
-					className='navbar-brand'
-					to={!isAuthenticated ? '/' : '/admin/dashboard'}>
-					{!restaurant || !restaurant.name ? '' : restaurant.name}
-				</Link>
-				<button
-					className='navbar-toggler'
-					type='button'
-					data-bs-toggle='collapse'
-					data-bs-target='#navbarSupportedContent'
-					aria-controls='navbarSupportedContent'
-					aria-expanded='false'
-					aria-label='Toggle navigation'>
-					<span className='navbar-toggler-icon'></span>
-				</button>
-				{isAuthenticated && !loading ? authLinks : guestLinks}
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link
+          className="navbar-brand"
+          to={!isAuthenticated ? "/" : "/admin/dashboard"}
+        >
+          {!restaurant || !restaurant.name ? "" : restaurant.name}
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        {isAuthenticated && !loading ? authLinks : guestLinks}
+      </div>
+    </nav>
+  );
 };
 
 Navbar.propTypes = {
-	tableNum: PropTypes.string.isRequired,
-	auth: PropTypes.object.isRequired,
-	logout: PropTypes.func.isRequired,
+  tableNum: PropTypes.string.isRequired,
+  auth: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-	auth: state.auth.admin,
-	tableNum: state.auth.tableNum,
-	restaurant: state.restaurant.restaurant,
+  auth: state.auth.admin,
+  tableNum: state.auth.tableNum,
+  restaurant: state.restaurant.restaurant,
 });
 
 export default connect(mapStateToProps, { logout, getRestaurant })(
-	withRouter(Navbar),
+  withRouter(Navbar)
 );
--- src/components/routing/Routes.js
+++ src/components/routing/Routes.js
@@ -1,59 +1,59 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import GuestRoute from '../routing/GuestRoute';
-import PrivateRoute from '../routing/PrivateRoute';
-import Menu from '../menu/Menu';
-import Tray from '../tray/Tray';
-import AdminLogin from '../auth/AdminLogin';
-import AdminDashboard from '../layout/Dashboard';
-import NotFound from '../layout/NotFound';
-import Order from '../orders/Order';
-import Account from '../admin/Account';
-import Dishes from '../dish/Dishes';
-import EditDish from '../dish/EditDish';
-import AddDish from '../dish/AddDish';
-import Categories from '../category/Categories';
-import AddCategory from '../category/AddCategory';
-import EditCategory from '../category/EditCategory';
-import Restaurant from '../admin/Restaurant';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import GuestRoute from "../routing/GuestRoute";
+import PrivateRoute from "../routing/PrivateRoute";
+import Menu from "../menu/Menu";
+import Tray from "../tray/Tray";
+import AdminLogin from "../auth/AdminLogin";
+import AdminDashboard from "../layout/Dashboard";
+import NotFound from "../layout/NotFound";
+import Order from "../orders/Order";
+import Account from "../admin/Account";
+import Dishes from "../dish/Dishes";
+import EditDish from "../dish/EditDish";
+import AddDish from "../dish/AddDish";
+import Categories from "../category/Categories";
+import AddCategory from "../category/AddCategory";
+import EditCategory from "../category/EditCategory";
+import Restaurant from "../admin/Restaurant";
 
 const Routes = () => {
-	return (
-		<div>
-			<Switch>
-				<GuestRoute exact path='/catalog' component={Menu} />
-				<GuestRoute exact path='/cart' component={Tray} />
-				<Route exact path='/admin' component={AdminLogin} />
-				<PrivateRoute
-					exact
-					path='/admin/dashboard'
-					component={AdminDashboard}
-				/>
-				<PrivateRoute exact path='/admin/orders' component={Order} />
-				<PrivateRoute exact path='/admin/products' component={Dishes} />
-				<PrivateRoute
-					exact
-					path='/admin/products/edit/:id'
-					component={EditDish}
-				/>
-				<PrivateRoute exact path='/admin/products/add' component={AddDish} />
-				<PrivateRoute exact path='/admin/categories' component={Categories} />
-				<PrivateRoute
-					exact
-					path='/admin/categories/edit/:id'
-					component={EditCategory}
-				/>
-				<PrivateRoute
-					exact
-					path='/admin/categories/add'
-					component={AddCategory}
-				/>
-				<PrivateRoute exact path='/admin/account' component={Account} />
-				<PrivateRoute exact path='/admin/store' component={Restaurant} />
-				<Route component={NotFound} />
-			</Switch>
-		</div>
-	);
+  return (
+    <div>
+      <Switch>
+        <GuestRoute exact path="/catalog" component={Menu} />
+        <GuestRoute exact path="/cart" component={Tray} />
+        <Route exact path="/admin" component={AdminLogin} />
+        <PrivateRoute
+          exact
+          path="/admin/dashboard"
+          component={AdminDashboard}
+        />
+        <PrivateRoute exact path="/admin/orders" component={Order} />
+        <PrivateRoute exact path="/admin/products" component={Dishes} />
+        <PrivateRoute
+          exact
+          path="/admin/products/edit/:id"
+          component={EditDish}
+        />
+        <PrivateRoute exact path="/admin/products/add" component={AddDish} />
+        <PrivateRoute exact path="/admin/categories" component={Categories} />
+        <PrivateRoute
+          exact
+          path="/admin/categories/edit/:id"
+          component={EditCategory}
+        />
+        <PrivateRoute
+          exact
+          path="/admin/categories/add"
+          component={AddCategory}
+        />
+        <PrivateRoute exact path="/admin/account" component={Account} />
+        {/* <PrivateRoute exact path='/admin/store' component={Restaurant} /> */}
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  );
 };
 
 Routes.propTypes = {};
