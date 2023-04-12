8da15c7fb47fad9ae1dfe035b63b6fbe56b34757
Bhavya Muni <bhavyamuni@outlook.com>
2023-04-12 12:44:56 -0400

    edit dishes

--- src/components/admin/Account.js
+++ src/components/admin/Account.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 const Account = ({ user }) => {
-	const { name, email, role } = user;
+  const { name, email, role } = user;
 
-	return (
-		<div>
-			<h1> Admin Account</h1>
-			<h3>Name: {name}</h3>
-			<h3>email: {email}</h3>
-			<h3>Role : {role}</h3>
-		</div>
-	);
+  return (
+    <div>
+      <h1> Admin Account</h1>
+      <h3>Name: {name}</h3>
+      <h3>Email: {email}</h3>
+      <h3>Role: {role}</h3>
+    </div>
+  );
 };
 
 Account.propTypes = {
-	user: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-	user: state.auth.admin.user,
+  user: state.auth.admin.user,
 });
 
 export default connect(mapStateToProps)(Account);
--- src/components/dish/Dishes.js
+++ src/components/dish/Dishes.js
@@ -1,117 +1,118 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { getDishes } from '../../actions/dishes';
-import { getCategories } from '../../actions/categories';
-import DishItem from './DishItem';
+import React, { Fragment, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { getDishes } from "../../actions/dishes";
+import { getCategories } from "../../actions/categories";
+import DishItem from "./DishItem";
 
 const Dishes = ({ dishes, getDishes, categories, getCategories }) => {
-	const [search, setSearch] = useState('');
-	const [filter, setFilter] = useState('');
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
 
-	useEffect(() => {
-		getDishes();
-		if (categories.length === 0) return getCategories();
-	}, [getDishes, categories, getCategories]);
+  useEffect(() => {
+    getDishes();
+    if (categories.length === 0) return getCategories();
+  }, [getDishes, categories, getCategories]);
 
-	let filteredDishes = '';
+  let filteredDishes = "";
 
-	if (!search && filter) {
-		const data = dishes.filter((dish) => dish.category.type === filter);
-		filteredDishes = data.map((dish) => (
-			<DishItem key={dish._id} dish={dish} />
-		));
-	}
+  if (!search && filter) {
+    const data = dishes.filter((dish) => dish.category.type === filter);
+    filteredDishes = data.map((dish) => (
+      <DishItem key={dish._id} dish={dish} />
+    ));
+  }
 
-	if (search && !filter) {
-		const data = dishes.filter((dish) =>
-			dish.dishName.toLowerCase().includes(search.toLowerCase()),
-		);
+  if (search && !filter) {
+    const data = dishes.filter((dish) =>
+      dish.dishName.toLowerCase().includes(search.toLowerCase())
+    );
 
-		filteredDishes = data.map((dish) => (
-			<DishItem key={dish._id} dish={dish} />
-		));
-	}
+    filteredDishes = data.map((dish) => (
+      <DishItem key={dish._id} dish={dish} />
+    ));
+  }
 
-	return (
-		<Fragment>
-			<div className='headers'>
-				<div>
-					<h1> Products </h1>
+  return (
+    <Fragment>
+      <div className="headers">
+        <div>
+          <h1> Products </h1>
 
-					<div className='actions-field'>
-						<div className='filters'>
-							<select
-								className='form-select'
-								onChange={(e) => setFilter(e.target.value)}
-								value={filter}>
-								<option value='' defaultValue>
-									Select a category
-								</option>
-								{categories.map((category) => (
-									<option key={category._id} value={category.type}>
-										{category.type}
-									</option>
-								))}
-							</select>
-						</div>
+          <div className="actions-field">
+            <div className="filters">
+              <select
+                className="form-select"
+                onChange={(e) => setFilter(e.target.value)}
+                value={filter}
+              >
+                <option value="" defaultValue>
+                  Select a category
+                </option>
+                {categories.map((category) => (
+                  <option key={category._id} value={category.type}>
+                    {category.type}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-						<div className='searchArea'>
-							<form className='d-flex'>
-								<input
-									onChange={(e) => setSearch(e.target.value)}
-									className='form-control me-2'
-									type='search'
-									placeholder='Search'
-									aria-label='Search'
-								/>
-							</form>
-						</div>
-					</div>
-				</div>
+            <div className="searchArea">
+              <form className="d-flex">
+                <input
+                  onChange={(e) => setSearch(e.target.value)}
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
 
-				<Link to='/admin/products/add'>
-					<button className='btn btn-outline-primary btn-add'>
-						Add a Dish
-					</button>
-				</Link>
-			</div>
+        <Link to="/admin/products/add">
+          <button className="btn btn-outline-primary btn-add">
+            Add a Product
+          </button>
+        </Link>
+      </div>
 
-			<table className='table order-table'>
-				<thead>
-					<tr>
-						<th>Dish Name</th>
-						<th>Categories</th>
-						<th>Price</th>
-						<th>In Stock?</th>
-						<th>Featured?</th>
-						<th>Hot?</th>
-						<th>Edit Dish</th>
-						<th>Delete Dish</th>
-					</tr>
-				</thead>
+      <table className="table order-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Categories</th>
+            <th>Price</th>
+            <th>In Stock?</th>
+            <th>Featured?</th>
+            <th>Hot?</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
 
-				<tbody>
-					{!filter &&
-						!search &&
-						dishes.map((dish) => <DishItem key={dish._id} dish={dish} />)}
+        <tbody>
+          {!filter &&
+            !search &&
+            dishes.map((dish) => <DishItem key={dish._id} dish={dish} />)}
 
-					{filteredDishes}
-				</tbody>
-			</table>
-		</Fragment>
-	);
+          {filteredDishes}
+        </tbody>
+      </table>
+    </Fragment>
+  );
 };
 
 Dishes.propTypes = {
-	getDishes: PropTypes.func.isRequired,
-	dishes: PropTypes.array.isRequired,
+  getDishes: PropTypes.func.isRequired,
+  dishes: PropTypes.array.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-	dishes: state.dishes.dishes,
-	categories: state.categories.categories,
+  dishes: state.dishes.dishes,
+  categories: state.categories.categories,
 });
 
 export default connect(mapStateToProps, { getDishes, getCategories })(Dishes);
